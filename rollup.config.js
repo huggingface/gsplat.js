2afeb1c4d7b45f9c88f3cea18cb867b7463c7f51
dylanebert <dylan@huggingface.co>
2023-11-05 20:06:06 -0500

    update rollup dependencies, fix github actions

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
+import { terser } from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript2";
 import workerLoader from "rollup-plugin-web-worker-loader";
 import replace from "@rollup/plugin-replace";
 
