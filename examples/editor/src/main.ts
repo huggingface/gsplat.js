87b590ebe6b44f2258e5371b3edef18c53a28017
dylanebert <dylan@huggingface.co>
2023-12-21 13:09:51 -0500

    formatting

--- examples/editor/src/main.ts
+++ examples/editor/src/main.ts
@@ -42,7 +42,7 @@ async function selectFile(file: File) {
             (progress: number) => {
                 progressIndicator.value = progress * 100;
             },
-            format
+            format,
         );
         progressDialog.close();
     } else {
@@ -125,7 +125,7 @@ async function main() {
             await SPLAT.PLYLoader.LoadAsync(
                 url,
                 engine.scene,
-                (progress) => (progressIndicator.value = progress * 100)
+                (progress) => (progressIndicator.value = progress * 100),
             );
             progressDialog.close();
         } else {
